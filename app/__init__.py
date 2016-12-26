a6a71d6d322fc30fa54b12df15ee38c322efbd83
Brian Rotich <brian.rotich@andela.com>
2016-12-26 22:04:11 +0300

    [FEATURE] add url endpoints

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,13 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
-#from app.mod_index.controller import mod_index
+from app.mod_auth.controller import mod_auth
+from app.mod_bucketlists.controller import mod_bucketlists
 
 app = Flask(__name__)
 
 app.config.from_object('config')
 db = SQLAlchemy(app)
 
-#app.register_blueprint(mod_index)
+app.register_blueprint(mod_bucketlists)
+app.register_blueprint(mod_auth)
