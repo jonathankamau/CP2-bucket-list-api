5b95b7db0b4fe48b013084323d86993844f6ea0b
Brian Rotich <brian.rotich@andela.com>
2017-01-05 15:11:16 +0300

    [FEATURE] add logic to validate username and password

--- app/mod_auth/controller.py
+++ app/mod_auth/controller.py
@@ -10,16 +10,47 @@ mod_auth = Blueprint('auth', __name__, url_prefix='/auth')
 @mod_auth.route('/register/', methods=['POST'])
 def register():
     username = request.form.get('username')
+    password = request.form.get('password')
+
+    if not username:
+        return jsonify({
+            'error': {
+                'message': 'unable to create user',
+                'details': [
+                    {
+                        'target': 'username',
+                        'message': 'username data field missing/empty from POST request'
+                    }
+                ]
+            }
+        }), 400
+
+    if not password:
+        return jsonify({
+            'error': {
+                'message': 'unable to create user',
+                'details': [
+                    {
+                        'target': 'password',
+                        'message': 'password data field missing/empty from POST request'
+                    }
+                ]
+            }
+        }), 400
 
     if User.query.filter_by(username=username).scalar():
         return jsonify({
             'error': {
                 'message': 'unable to create user',
-                'details': 'username already registered'
+                'details': [
+                    {
+                        'target': 'username',
+                        'message': 'username already registered'
+                    }
+                ]
             }
         }), 400
 
-    password = request.form.get('password')
     user = User(username, password)
     user.save()
     user.refresh_from_db()
