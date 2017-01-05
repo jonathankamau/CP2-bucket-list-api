6ff25eaa85514ff15410246be1c5b15a4db08bb6
Brian Rotich <brian.rotich@andela.com>
2017-01-05 23:03:33 +0300

    [FEATURE] add get user bucket list logic

--- app/mod_bucketlists/controller.py
+++ app/mod_bucketlists/controller.py
@@ -1,12 +1,14 @@
-from flask import Blueprint
+from flask import Blueprint, request, jsonify
 from flask_httpauth import HTTPTokenAuth
 from itsdangerous import BadTimeSignature, BadSignature
 
 from app import token_signer
+from app.mod_bucketlists.models import BucketList
 
 mod_bucketlists = Blueprint('bucketlists', __name__, url_prefix='/bucketlists')
 auth = HTTPTokenAuth('Token')
 
+user_id = 1
 
 @auth.verify_token
 def verify_token(token):
@@ -14,16 +16,30 @@ def verify_token(token):
      must return True or False"""
     if token:  # TODO fix the token validation clause
         try:
-            user_name, time_created = token_signer.unsign(token, return_timestamp=True)
+            user_name, time_created = token_signer.unsign(token, return_timestamp=True)  # TODO get user id
+            return True
         except (BadTimeSignature, BadSignature)as e:
-            return False
-        return True
+            return True  # TODO return message on token failure
+
     return False
 
 
 @mod_bucketlists.route('/', methods=['GET', 'POST'])
-@auth.verify_token
 def get_bucketlists():
+    if request.method == 'GET':
+        bucket_lists = BucketList.query.filter_by(created_by=user_id).all()
+
+        return jsonify({
+            'data': [
+                {
+                    'id': bucket_list.id,
+                    'name': bucket_list.name,
+                    'created_by': bucket_list.created_by,
+                    'date_created': bucket_list.date_created,
+                    'date_modified': bucket_list.date_modified
+                } for bucket_list in bucket_lists
+                ]
+        })
     return "not implmented"
 
 
