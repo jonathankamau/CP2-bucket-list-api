4704c28b08b4cf617ec314caabda713510fc4c50
Brian Rotich <brian.rotich@andela.com>
2017-01-05 22:49:31 +0300

    [TEST] fixed errors on mod_bucketlist tests

--- app/mod_bucketlists/tests/test_single_bucketlist.py
+++ app/mod_bucketlists/tests/test_single_bucketlist.py
@@ -44,7 +44,7 @@ class SingleBucketListTestCase(BaseTestCase):
 
     def test_updates_bucketlist_name(self):
         data = {'name': 'New Bucket'}
-        response = self.client.post('/bucketlists/1', data=data, headers=self.token, follow_redirects=True)
+        response = self.client.put('/bucketlists/1', data=data, headers=self.token, follow_redirects=True)
 
         self.assertEqual(200, response.status_code)
 
@@ -54,7 +54,7 @@ class SingleBucketListTestCase(BaseTestCase):
 
     def test_error_on_updating_bucketlist_name_to_existing_name(self):
         data = {'name': 'Checkpoint'}
-        response = self.client.post('/bucketlists/1', data=data, headers=self.token, follow_redirects=True)
+        response = self.client.put('/bucketlists/1', data=data, headers=self.token, follow_redirects=True)
 
         self.assertEqual(400, response.status_code)
 
@@ -66,7 +66,7 @@ class SingleBucketListTestCase(BaseTestCase):
     def test_deletes_users_bucketlist(self):
         response = self.client.delete('/bucketlists/1', headers=self.token, follow_redirects=True)
 
-        self.assertEqual(400, response.status_code)
+        self.assertEqual(200, response.status_code)
 
         response = response.data.decode('utf-8')
         self.assertIn('data', response)
